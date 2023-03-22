1efe4512bf2a833ec0ad3eb15707b1fb5afbf04c
vivntng <vltang@ucsd.edu>
2023-03-21 18:22:04 -0700

    Refactor index.js to have BrowserRouter basename={process.env.PUBLIC_URL} for gh deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/HomePage';
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />}/>
     </Routes>
   );
 }
